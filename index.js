276e3070a279ed809abb278a2d64f38f1cbb4d8b
kj3004 <137755543+kj3004@users.noreply.github.com>
2024-07-18 23:38:18 +0530

    Tour,User backends

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
+import tourRoute from './routes/tours.js';
+import userRoute from './routes/users.js';
+
 dotenv.config()
 const app = express ()
 const port = process.env.PORT || 8000;
@@ -29,6 +32,8 @@ const connect = async()=>{
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use('/tours' , tourRoute);
+app.use('/users' , userRoute);
 
 
 app.listen(port, ()=>{
